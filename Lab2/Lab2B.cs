0553cd2f6b467817c0ea356ab22c2c73e322a1c1
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2022-07-29 13:58:40 -0400

    Lab2B

---
+++ Lab2/Lab2B.cs
@@ -0,0 +1,31 @@
+﻿
+/*
+   Class: 1321L
+   Section: Section W#1 C#
+   Term: Spring 2022
+   Instructor: Gayathri Yella
+   Name: Brandon Wright
+   Lab#: 2
+*/
+
+using System;
+
+namespace Lab2
+{
+    class Lab2B
+    {
+        static void Main(string[] args)
+        {
+            string uI;
+
+            Console.WriteLine("___*___");
+            Console.WriteLine("__*_*__");
+            Console.WriteLine("_*_*_*_");
+            Console.WriteLine("*_*_*_*");
+            Console.WriteLine("_*_*_*_");
+            Console.WriteLine("__*_*__");
+            Console.WriteLine("___*___");
+
+        }
+    }
+}
